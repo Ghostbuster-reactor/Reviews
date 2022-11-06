812774b9ab425df047fb820c3ca8730e352b2c52
RandyIp <randyip9@mail.com>
2022-11-06 14:38:38 -0800

    Finished most API end points except meta

--- database/db.js
+++ database/db.js
@@ -8,40 +8,41 @@ const pool = new Pool({
 })
 
 // set up, set to false to turn off, comment things out if you only need specifics
-if (true) {
-  // pool.query(`CREATE TABLE IF NOT EXISTS reviews (
-  // review_id SERIAL PRIMARY KEY NOT NULL, product_id INT, rating INT, date BIGINT, summary VARCHAR(500), body VARCHAR(1000), recommend BOOLEAN, reported BOOLEAN DEFAULT false, reviewer_name VARCHAR(500), reviewer_email VARCHAR(500), response VARCHAR(1000), helpfulness INT DEFAULT 0
-  // );`, (err) => { if (err) throw err })
+if (false) {
+  pool.query(`CREATE TABLE IF NOT EXISTS reviews (
+  review_id SERIAL PRIMARY KEY NOT NULL, product_id INT, rating INT, date BIGINT, summary VARCHAR(500), body VARCHAR(1000), recommend BOOLEAN, reported BOOLEAN DEFAULT false, reviewer_name VARCHAR(500), reviewer_email VARCHAR(500), response VARCHAR(1000), helpfulness INT DEFAULT 0
+  );`, (err) => {
+    if (err) throw err
+    pool.query(`CREATE INDEX IF NOT EXISTS reviews_index ON reviews(product_id)`)
+    // COPY reviews(review_id, product_id, rating, date, summary, body, recommend, reported, reviewer_name, reviewer_email, response, helpfulness) FROM '/Users/randy/hackreactor/SDC/CSV/reviews.csv' DELIMITER ',' CSV HEADER;
 
-  // COPY reviews(review_id, product_id, rating, date, summary, body, recommend, reported, reviewer_name, reviewer_email, response, helpfulness) FROM '/Users/randy/hackreactor/SDC/CSV/reviews.csv' DELIMITER ',' CSV HEADER;
+    // SELECT setval('reviews_review_id_seq', (SELECT MAX(review_id) FROM reviews)+1);
+  })
 
-  // pool.query(`CREATE TABLE IF NOT EXISTS meta (
-  // id SERIAL PRIMARY KEY NOT NULL, characteristic_id INT, review_id INT REFERENCES reviews (review_id), value INT
-  // )`, (err) => { if (err) throw err })
+  pool.query(`CREATE TABLE IF NOT EXISTS photos(
+    id SERIAL PRIMARY KEY NOT NULL, review_id INT REFERENCES reviews (review_id), url VARCHAR(200)
+  )`, (err) => {
+    if (err) throw err
+    pool.query(`CREATE INDEX IF NOT EXISTS photos_index ON photos (review_id)`, (err) => { if (err) throw err })
+    // COPY photos (id, review_id, url) FROM '/Users/randy/hackreactor/SDC/CSV/reviews_photos.csv' DELIMITER ',' CSV HEADER;
 
-  //COPY meta(id, characteristic_id, review_id, value)
-  //FROM '/Users/randy/hackreactor/SDC/CSV/characteristic_reviews.csv' DELIMITER ',' CSV HEADER;
+    //SELECT setval('photos_id_seq', (SELECT MAX(id) FROM photos)+1);
+  })
 
-  // pool.query(`CREATE TABLE IF NOT EXISTS photos(
-  //   id SERIAL PRIMARY KEY NOT NULL, review_id INT REFERENCES reviews (review_id), url VARCHAR(200)
-  // )`, (err) => { if (err) throw err })
+  pool.query(`CREATE TABLE IF NOT EXISTS meta (
+    id SERIAL PRIMARY KEY NOT NULL, characteristic_id INT, review_id INT REFERENCES reviews (review_id), value INT
+    )`, (err) => {
+    if (err) throw err
+    // COPY meta(id, characteristic_id, review_id, value) FROM '/Users/randy/hackreactor/SDC/CSV/characteristic_reviews.csv' DELIMITER ',' CSV HEADER;
 
-  // COPY photos (id, review_id, url)
-  // FROM '/Users/randy/hackreactor/SDC/CSV/reviews_photos.csv' DELIMITER ',' CSV HEADER;
+    //SELECT setval('meta_id_seq', (SELECT MAX(id) FROM meta)+1);
+  })
 
-  // pool.query(`SELECT review_id FROM reviews`, (err, result1) => {
-  //   if (err) throw err
-  //   for (i of result1.rows) {
-  //     pool.query(`SELECT id, url FROM photos WHERE review_id = ${i.review_id}`, (err, result2) => {
-  //       if (err) throw err
-  //       if (result2.rows.length > 0) {
-  //         let test = `ARRAY['{"id":${result2.rows[0].id}, "url": "${result2.rows[0].url}"}']`
-  //         console.log(test)
-  //         pool.query(`UPDATE reviews SET photos = array_append(photos,${test}) WHERE review_id = ${i.review_id}`)
-  //       }
-  //     })
-  //   }
-  //   console.log('photos updated successfully')
-  // })
+  pool.query(`CREATE TABLE IF NOT EXISTS characteristics(id SERIAL PRIMARY KEY NOT NULL, product_id INT, name VARCHAR(100))`,
+    (err) => {
+      if (err) throw err
+      // COPY characteristics FROM '/Users/randy/hackreactor/SDC/CSV/characteristics.csv' DELIMITER ',' CSV HEADER;
+    })
+  // ALTER TABLE questions ALTER COLUMN question_date TYPE TIMESTAMP USING to_timestamp(question_date/1000);
 }
 module.exports = pool
--- server/Controllers/reviews.js
+++ server/Controllers/reviews.js
@@ -1,49 +1,91 @@
 const pool = require('../../database/db.js')
 
 const reviews = {
+  // ----------------------------------------- GET REQUEST -----------------------------------------
   get: async (req, res) => {
+
+    // set sort parameter to be a column name if listed
     if (!req.query.sort) req.query.sort = 'review_id'
     if (req.query.sort === 'helpful') req.query.sort = 'helpfulness'
     if (req.query.sort === 'newest') req.query.sort = 'date'
+
+    // create parameters
     const queryArgs = [req.query.product_id, req.query.page || 0, req.query.count || 5]
+
+    // try query
     try {
-      result = await pool.query(`SELECT review_id, rating, summary, recommend, response, body, to_timestamp(date/1000) as date, reviewer_name, helpfulness FROM reviews
-      WHERE product_id = $1
+      const result = await pool.query(`
+      SELECT review_id, rating, summary, recommend, response, body, to_timestamp(date/1000) as date, reviewer_name, helpfulness,
+        (SELECT json_agg(json_build_object('id', photos.id, 'url', photos.url)) FROM photos WHERE review_id = reviews.review_id
+        ) as photos, reported FROM reviews
+      WHERE product_id = $1 AND reported = FALSE
       ORDER BY ${req.query.sort} DESC
       OFFSET $2
       LIMIT $3`, queryArgs)
-      for (let i = 0; i < result.rows.length; i++) {
-        result.rows[i].photos = pool.query(`SELECT id, url FROM photos WHERE review_id = ${result.rows[i].review_id}`)
-      }
+
+      // end response
       res.status(200)
       res.send(result.rows)
     } catch (e) { console.log(e) }
   },
+
+  // ----------------------------------------- POST REQUEST -----------------------------------------
   post: async (req, res) => {
     let params1 = [req.body.product_id, req.body.rating, req.body.summary || null, req.body.body || null, req.body.recommend, req.body.name, req.body.email]
     try {
       pool.query(`INSERT INTO reviews (review_id, product_id, rating, summary, body, recommend, reviewer_name, reviewer_email)
     VALUES (nextval('reviews_review_id_seq'::regclass), $1,$2,$3,$4,$5,$6,$7)`, params1)
       const response = await pool.query(`SELECT MAX(review_id) FROM reviews`)
-      pool.query(`INSERT INTO photos (review_id, url) VALUES ($1,$2)`, [response.rows[0].review_id, req.body.photos])
+      for (let i = 0; i < req.body.photos.length; i++) {
+        pool.query(`INSERT INTO photos (review_id, url) VALUES ($1,$2)`, [response.rows[0].max, req.body.photos[i]])
+      }
+      let meta_keys = Object.keys(req.body.characteristics)
+      for (i of meta_keys) {
+        pool.query(`INSERT INTO meta (characteristic_id, review_id, value) VALUES ($1,$2,$3)`, [Number(i), response.rows[0].max, req.body.characteristics[i]])
+      }
       res.status(201)
       res.send('CREATED!')
-      // pool.query(`INSERT INTO meta (characteristic_id, review_id, value) VALUES ($1,$2,$3)`, [response.rows[0].review_id, req.body.photos] )
+
     } catch (e) { console.log(e) }
   },
-  meta: (req, res) => {
-    pool.query(`SELECT SOMETHING FROM SOMETHING WHERE product_id = $1`[req.query.product_id])
+
+  // ----------------------------------------- META REQUEST -----------------------------------------
+  meta: async (req, res) => {
+    const result = await pool.query(`
+      SELECT ${req.query.product_id} as product_id, json_build_object (
+        '1', (SELECT COUNT (rating) FROM reviews WHERE rating = 1 AND product_id = ${req.query.product_id}),
+        '2', (SELECT COUNT (rating) FROM reviews WHERE rating = 2 AND product_id = ${req.query.product_id}),
+        '3', (SELECT COUNT (rating) FROM reviews WHERE rating = 3 AND product_id = ${req.query.product_id}),
+        '4', (SELECT COUNT (rating) FROM reviews WHERE rating = 4 AND product_id = ${req.query.product_id}),
+        '5', (SELECT COUNT (rating) FROM reviews WHERE rating = 5 AND product_id = ${req.query.product_id})
+        ) as ratings,
+        json_build_object (
+          'false', (SELECT COUNT (recommend) FROM reviews WHERE recommend = false AND product_id = ${req.query.product_id}),
+          'true', (SELECT COUNT (recommend) FROM reviews WHERE recommend = true AND product_id = ${req.query.product_id})
+        ) as recommended,
+        json_build_object (
+
+        ) as characteristics`)
+    res.status(200)
+    res.send(result.rows[0])
   },
+
+
+  // 'ratings', json_build_object(
+  //   '1', (SELECT COUNT (rating) FROM reviews WHERE rating = 1))
+  // ----------------------------------------- HELPFUL REQUEST -----------------------------------------
   helpful: (req, res) => {
-    pool.query(`UPDATE reviews SET helpfulness = helpfulness + 1 WHERE review_id=$1`, [req.params.review_id])
-    res.status(204)
-    res.send('NO CONTENT')
+    pool.query(`UPDATE reviews SET helpfulness = helpfulness + 1 WHERE review_id = $1`, [req.params.review_id])
+    res.status(204).end()
   },
+
+  // ----------------------------------------- REPORT REQUEST -----------------------------------------
   report: (req, res) => {
-    pool.query(`UPDATE reviews SET reported = true WHERE review_id=$1`, [req.params.review_id])
-    res.status(204)
-    res.send('NO CONTENT')
+    pool.query(`UPDATE reviews SET reported = true WHERE review_id = $1`, [req.params.review_id])
+      .then(() => res.status(204).send('lets make sure this works'))
   },
+
+  // ----------------------------------------- TEST REQUEST -----------------------------------------
   test: async (req, res) => {
     let response
     try {
