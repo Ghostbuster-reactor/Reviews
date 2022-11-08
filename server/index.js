8c463a7f4a90847f5c50d5116cf621f35c354d70
RandyIp <randyip9@mail.com>
2022-11-08 12:09:51 -0800

    Adding loader.io

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 app.use('/', router)
+app.get('http://52.26.228.170:1128/loaderio-12f7a3c1f129dc82f35cb36acf434d3d.html')
 
 
 app.listen(port, function () {
